eaffb00ffbfd8a9626a745e59be69b947b5450c4
tvl2386 <drabber2000@gmail.com>
2025-08-14 22:47:23 +0200

    changed logo and added navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="w-full h-16 md:h-20 flex items-center justify-between">
+      {/* LOGO */}
+      <div className="flex items-center gap-4 text-2xl font-bold">
+        <img
+          src="/ae2craft-logo.png"
+          className="w-16 h-16 drop-shadow-[0_0_6px_rgba(255,109,0,0.5)]"
+          alt="AE2Craft Logo"
+        />
+        <span>P2P Craft</span>
+      </div>
+
+      {/* MOBILE MENU */}
+      <div className="md:hidden">
+        {/* MOBILE BUTTON */}
+        <div
+          className="cursor-pointer text-4xl"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          {open ? "X" : "☰"}
+        </div>
+        {/* MOBILE LINK LIST */}
+        <div
+          className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out ${
+            open ? "-right-0" : "-right-[100%]"
+          }`}
+        >
+          <a href="/">Home</a>
+          <a href="/">Trending</a>
+          <a href="/">Most Popular</a>
+          <a href="/">About</a>
+          <a href="/">
+            <button className="py-2 px-4 round-3xl bg-blue-800 text-white">
+              Login{" "}
+            </button>
+          </a>
+        </div>
+      </div>
+
+      {/* DESKTOP MENU */}
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <a href="/">Home</a>
+        <a href="/">Trending</a>
+        <a href="/">Most Popular</a>
+        <a href="/">About</a>
+        <a href="/">
+          <button className="py-2 px-4 round-3xl bg-blue-800 text-white">
+            Login{" "}
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
